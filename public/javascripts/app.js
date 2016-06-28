d02400619afe657a1641465cc2a34c6d9188b5f3
Matt Lane <lane.matthew@gmail.com>
2016-06-28 14:35:33 -0700

    add d3

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,6 +4,7 @@ $(function() {
   var $msgInput = $("#message-input");
   var $messages = $("#messages");
   var socket = io();
+  var dataSet = [];
 
   $msgForm.on('submit', function(e) {
     e.preventDefault();
@@ -31,6 +32,42 @@ $(function() {
     });
     $newLi.append($displayedData);
     $messages.append($newLi);
+
+    dataSet.push(data);
+    updateGraph();
   });
+
+  // set up svg
+  var margin = {top: 30, right: 20, bottom: 30, left: 50};
+  var width = 600 - margin.left - margin.right;
+  var height = 600 - margin.top - margin.bottom;
+
+  var svg = d3.select('#graph')
+              .append('svg')
+              .attr('height', height)
+              .attr('width', width)
+
+  var xScale = d3.scaleLinear().domain([-1,1]).range([margin.left,width - margin.right]);
+  var yScale = d3.scaleLinear().domain([0,1]).range([height - margin.top,margin.bottom]);
+
+  function updateGraph() {
+    svg.selectAll('circle')
+      .data(dataSet)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d) {
+        return xScale(d.polarity);
+      })
+      .attr('cy', function(d) {
+        return yScale(d.subjectivity);
+      })
+      .attr('fill', '#2aabd2')
+      .attr('stroke', '#265a88')
+      .attr('r', function(d) {
+        return Math.max(3,Math.sqrt(d.length)/10);
+      })
+  }
+        
+  updateGraph();
 
 });

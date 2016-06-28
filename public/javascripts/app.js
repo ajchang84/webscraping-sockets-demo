732d7d97f3cc5c58305b592e9e17b60f6d1baac2
Matt Lane <lane.matthew@gmail.com>
2016-06-28 10:42:59 -0700

    semicolons and form tweak

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,18 +1,18 @@
 $(function() {
 
-  var $msgForm = $("#new-message form")
-  var $msgInput = $("#message-input")
-  var $messages = $("#messages")
+  var $msgForm = $("#new-message form");
+  var $msgInput = $("#message-input");
+  var $messages = $("#messages");
   var socket = io();
 
   $msgForm.on('submit', function(e) {
     e.preventDefault();
-    socket.emit('message', $msgInput.val())
+    socket.emit('message', $msgInput.val());
     $msgInput.val('');
   });
 
   socket.on('message', function(newMessage) {
-    $messages.append($('<li></li>', {text: newMessage, class: 'list-group-item'}))
+    $messages.append($('<li></li>', {text: newMessage, class: 'list-group-item'}));
   });
 
 });
